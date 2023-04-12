c2794868236f3c42a235dd922d8fd69197136114
Jerry W. Moody <waylandmoody55@gmail.com>
2023-04-12 18:37:50 -0500

    Added schema.php file that will update/create the database information. only run the file when you need the database to be updated.

---
+++ src/config/schema.php
@@ -0,0 +1,356 @@
+<?php
+require_once 'config.php';
+
+// Create connection
+$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create the database if it doesn't exist
+$sql = "CREATE DATABASE IF NOT EXISTS amplify";
+if ($conn->query($sql) !== true) {
+    die("Error creating database: " . $conn->error);
+}
+
+// Select the database
+$conn->select_db(DB_NAME);
+//-- Table structure for table `songs`
+//--
+
+// Create the tables if they don't exist
+$sql = "DROP TABLE IF EXISTS album_artists, song_artists, song_playlists, admins, reviews, artists, songs, playlists, users, albums;
+
+CREATE TABLE `admins` (
+  `admin_id` int NOT NULL,
+  `admin_password` int NOT NULL,
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `albums`
+--
+
+CREATE TABLE `albums` (
+  `album_id` int NOT NULL,
+  `album_title` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `release_date` date NOT NULL,
+  `release_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album_artists`
+--
+
+CREATE TABLE `album_artists` (
+  `album_artists_id` int NOT NULL,
+  `album_id` int NOT NULL,
+  `artist_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artists`
+--
+
+CREATE TABLE `artists` (
+  `artist_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `stage_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE `playlists` (
+  `playlist_id` int NOT NULL,
+  `user_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `review_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `song_id` int DEFAULT NULL,
+  `album_id` int DEFAULT NULL,
+  `comment` text COLLATE utf8mb4_general_ci NOT NULL,
+  `rating` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `songs`
+--
+
+CREATE TABLE `songs` (
+  `song_id` int NOT NULL,
+  `song_title` int NOT NULL,
+  `length` time NOT NULL,
+  `listens` int NOT NULL,
+  `album_id` int DEFAULT NULL,
+  `release_date` int NOT NULL,
+  `release_time` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `song_artists`
+--
+
+CREATE TABLE `song_artists` (
+  `song_artist_id` int NOT NULL,
+  `song_id` int NOT NULL,
+  `artist_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `song_playlists`
+--
+
+CREATE TABLE `song_playlists` (
+  `song_playlist_id` int NOT NULL,
+  `playlist_id` int NOT NULL,
+  `song_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL,
+  `username` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `first_name` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `last_name` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(320) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `first_name`, `last_name`, `email`) VALUES
+(1, 'deez', 'ben_dover', 'Rohan', 'Dhawan', 'rohan@gmail.com'),
+(2, 'nutz', 'yes', 'Wayland', 'Moody', 'wayland@gmail.com'),
+(3, 'got_eem', 'nope', 'Tathluach', 'Chol', 'tathluach@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD KEY `Admin to User` (`user_id`);
+
+--
+-- Indexes for table `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`album_id`);
+
+--
+-- Indexes for table `album_artists`
+--
+ALTER TABLE `album_artists`
+  ADD PRIMARY KEY (`album_artists_id`),
+  ADD KEY `AA to Album` (`album_id`),
+  ADD KEY `AA to Artist` (`artist_id`);
+
+--
+-- Indexes for table `artists`
+--
+ALTER TABLE `artists`
+  ADD PRIMARY KEY (`artist_id`),
+  ADD KEY `Artist to User` (`user_id`);
+
+--
+-- Indexes for table `playlists`
+--
+ALTER TABLE `playlists`
+  ADD PRIMARY KEY (`playlist_id`),
+  ADD KEY `Playlist User` (`user_id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD KEY `Review to User` (`user_id`),
+  ADD KEY `Review to Song` (`song_id`),
+  ADD KEY `Review to Album` (`album_id`);
+
+--
+-- Indexes for table `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`song_id`),
+  ADD KEY `Song to Album` (`album_id`);
+
+--
+-- Indexes for table `song_artists`
+--
+ALTER TABLE `song_artists`
+  ADD PRIMARY KEY (`song_artist_id`),
+  ADD KEY `SA to Song` (`song_id`),
+  ADD KEY `SA to Artist` (`artist_id`);
+
+--
+-- Indexes for table `song_playlists`
+--
+ALTER TABLE `song_playlists`
+  ADD PRIMARY KEY (`song_playlist_id`),
+  ADD KEY `SP to Playlist` (`playlist_id`),
+  ADD KEY `SP to Song` (`song_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `password` (`password`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username_2` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `album_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `album_artists`
+--
+ALTER TABLE `album_artists`
+  MODIFY `album_artists_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `artists`
+--
+ALTER TABLE `artists`
+  MODIFY `artist_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `playlists`
+--
+ALTER TABLE `playlists`
+  MODIFY `playlist_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `song_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `song_artists`
+--
+ALTER TABLE `song_artists`
+  MODIFY `song_artist_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `song_playlists`
+--
+ALTER TABLE `song_playlists`
+  MODIFY `song_playlist_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admins`
+--
+ALTER TABLE `admins`
+  ADD CONSTRAINT `Admin to User` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `album_artists`
+--
+ALTER TABLE `album_artists`
+  ADD CONSTRAINT `AA to Album` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `AA to Artist` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`artist_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `artists`
+--
+ALTER TABLE `artists`
+  ADD CONSTRAINT `Artist to User` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `playlists`
+--
+ALTER TABLE `playlists`
+  ADD CONSTRAINT `Playlist User` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `Review to Album` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `Review to Song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `Review to User` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `Song to Album` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`) ON DELETE SET NULL ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `song_artists`
+--
+ALTER TABLE `song_artists`
+  ADD CONSTRAINT `SA to Artist` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`artist_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `SA to Song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `song_playlists`
+--
+ALTER TABLE `song_playlists`
+  ADD CONSTRAINT `SP to Playlist` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`playlist_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `SP to Song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+COMMIT;";
+
+// Execute the query
+if ($conn->multi_query($sql) === TRUE) {
+    echo "database created";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Close the connection
+$conn->close();
+?>
