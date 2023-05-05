9b07ca23ba9b70a416e9a26c6cea68c6192727a0
irving <irvingsalinas@tamu.edu>
2023-05-05 13:27:55 -0500

    works

--- public/loginPage.php
+++ public/loginPage.php
@@ -1,5 +1,38 @@
-<?php?>
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Connect to database
+    $servername = "localhost";
+    $username = "your_username";
+    $password = "your_password";
+    $dbname = "amplify";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
+    // Get username and password from POST request
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // SQL query to check if username and password combination is valid
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        // Valid username and password combination
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        header("Location: homePage.php");
+        exit;
+    } else {
+        // Invalid username and password combination
+        $error = "Invalid username or password";
+    }
+
+    $conn->close();
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -20,13 +53,33 @@
 <body>
 <div class="center">
     <h1>Login</h1>
-    <form method="post" action="login.php">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br><br>
-        <label for="password">Password:</label>
-        <input type="password" name="password" required><br><br>
-        <input type="submit" value="Login">
+    <?php if (isset($error)) { ?>
+        <div style="color: red;"><?php echo $error; ?></div>
+    <?php } ?>
+    <form method="post">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+        </div>
+        <button type="submit">Login</button>
     </form>
 </div>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/signup.php
+++ public/signup.php
@@ -9,8 +9,22 @@ $password = $_POST['password'];
 $confirm_password = $_POST['confirm_password'];
 $artist = isset($_POST['artist']) ? 1 : 0;
 
-// TODO: Validate the username, email, and password
-// For example, you might check if the email is valid, if the username is unique, and if the password meets certain criteria
+// Validate the username, email, and password
+if (empty($username) || !preg_match('/^[a-zA-Z0-9_]+$/', $username)) {
+    $_SESSION['error'] = 'Invalid username. Only letters, numbers, and underscores are allowed.';
+    header('Location: signup.php');
+    exit();
+}
+if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $_SESSION['error'] = 'Invalid email address.';
+    header('Location: signup.php');
+    exit();
+}
+if (empty($password) || strlen($password) < 8) {
+    $_SESSION['error'] = 'Password must be at least 8 characters long.';
+    header('Location: signup.php');
+    exit();
+}
 
 // Check if the password and confirm password fields match
 if ($password !== $confirm_password) {
@@ -19,20 +33,24 @@ if ($password !== $confirm_password) {
     exit();
 }
 
-// TODO: Hash the password and store the user's data in a database
-// For example, you might use the PHP password_hash() function to securely hash the password
+// Hash the password and store the user's data in a database
 $pdo = new PDO('mysql:host=localhost;dbname=song_streaming_website', 'username', 'password');
 
-// insert the user into the database
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
+
 $stmt = $pdo->prepare('INSERT INTO users (username, password, email, is_artist) VALUES (?, ?, ?, ?)');
-$stmt->execute([$username, $password, $email, $artist]);
+if (!$stmt->execute([$username, $password_hash, $email, $artist])) {
+    $_SESSION['error'] = 'Error inserting user into database.';
+    header('Location: signup.php');
+    exit();
+}
+
 // Store the user's data in the session and redirect to the home page
 $_SESSION['username'] = $username;
 $_SESSION['email'] = $email;
 $_SESSION['loggedin'] = true;
 header('Location: homePage.php');
 exit();
-
 
 // get the form data
 //$username = $_POST['username'];
